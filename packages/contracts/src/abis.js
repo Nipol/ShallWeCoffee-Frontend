e20df7105b8b7ef87008900bfe52dfc65d954786
yoonsung.eth <ys.choi@me.com>
2020-12-08 20:34:16 +0900

    feat: add tokenfactory info (#1)

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,9 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
+import tokenFactoryABI from "./abis/tokenfactory.json";
 
 const abis = {
-  erc20: erc20Abi,
-  ownable: ownableAbi,
+  tokenfactory: tokenFactoryABI
 };
 
 export default abis;
--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,7 +1,7 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  tokenFactory: "0xCc83Dd82FB74B5056De897B68244D0895a9a07e4",
 };
 
 export default addresses;
