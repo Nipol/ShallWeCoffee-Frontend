125dbcf1f86196d91d2a4dbdc3727bc9c8f603d4
Yoonsung <ys.choi@me.com>
2020-12-11 10:44:10 +0900

    final

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,8 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ENS_REGISTRY: "0x112234455C3a32FD11230C42E7Bccd4A84e02010",
-  MANAGER_FACTORY: "0xaa13a4C86C410f97756fE0d76d07FB644feB2799"
+  MANAGER_FACTORY: "0xFc80d8E30E295D567d430dE6C1C2357AFA1109ed",
+  MANAGER_TEMPLATE: "0x26a26Aa3F5F0Bbb21D21520076a4cf71745B1C07"
 };
 
 export default addresses;
